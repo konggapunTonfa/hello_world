3a197b7b4eff8e29908802a1689567439a4d20ad
konggapunTonfa <tonfa@users.noreply.github.com>
2018-10-16 21:26:01 -0400

    Update hello.java
    forgot to leave a comment
    changed values of strings firstName and lastName

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Konggapun";
+        String firstName = "Jimmy";
         String lastName = "Tonfa";
 	
         System.out.println("Hello Class!");
