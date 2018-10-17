0828fbb08cd8b174d65c3445f07e90e9bb0ff1fe
konggapunTonfa <tonfa@users.noreply.github.com>
2018-10-16 21:24:58 -0400

    git status

--- hello.java
+++ hello.java
@@ -5,8 +5,8 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Konggapun";
+        String lastName = "Tonfa";
 	
         System.out.println("Hello Class!");
 
